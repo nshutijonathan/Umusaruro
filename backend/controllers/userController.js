b0fb56a8b96f862878a7ccd6686ae5279fe7fc56
nshutijonathan <nshutijonathan130@gmail.com>
2021-09-28 20:10:53 +0200

    fixed-login-back-end

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
+
 import User from "../models/userModel.js";
+import Usershelpers from "../helpers/user.js";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -9,7 +11,8 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  if (user && (await user.matchPassword(password))) {
+  console.log("usersss", user.password);
+  if (user && Usershelpers.comparePassword(user.password, password)) {
     res.json({
       _id: user._id,
       name: user.name,
